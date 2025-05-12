e79cb3e73f9a8af4007f891e04be7366c0ce286b
Shaun Adel <shahan001@gmail.com>
2025-05-13 09:11:11 +1000

    remove Datecounter for now

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import DateCounter from './DateCounter';
 export default function App() {
     return (
         <div>
-            <DateCounter />
             <h1>Welcome to the Date Counter App</h1>
         </div>
     );
